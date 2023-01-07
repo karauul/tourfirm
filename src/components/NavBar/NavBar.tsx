6feb93aeb2b30744ad960d239e04f094d3cd10bc
karauul <adel_121@mail.ru>
2023-01-07 14:47:13 +0300

    поправили скроллинг к секциям

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -11,11 +11,21 @@ interface IProps {
 }
 
 const NavBar: React.FC<IProps> = (props: IProps) => {
+  const scrollToElement = (el: HTMLElement) => {
+    const headerOffset = window.innerHeight * 0.1;
+    const rootNode = document.getElementById('root'[0]);
+    const elementPositionFromTop = el.offsetTop - headerOffset;
+    window.scrollTo({
+      top: elementPositionFromTop,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <div className="nav-bar">
       {props.items.map((item) => (
         <div key={item.title} className="nav">
-          <HashLink to={item.link} smooth>
+          <HashLink to={item.link} scroll={scrollToElement}>
             {item.title}
           </HashLink>
         </div>
